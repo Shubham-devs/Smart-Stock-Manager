4d43f7dde24d67ee3a0935ca97cfd14b42584d92
Shubham-devs <snpjogi123@gmail.com>
2026-02-06 09:40:15 +0530

    updated frontend API connection

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+// alert(import.meta.env.VITE_API_URL)
+
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    timeout: 3000,
+});
+
+// Request interceptor
+instance.interceptors.request.use(
+    async (config) => {
+        try {
+            const accessToken = localStorage.getItem("userToken")
+            // console.log("kjhfjsd", accessToken)
+            config.headers.Authorization = `Bearer ${accessToken}`;
+            return config;
+        } catch (error) {
+            console.error("Error setting Authorization header:", error);
+            return Promise.reject(error);
+        }
+    },
+    (error) => {
+        console.error("Request error:", error);
+        return Promise.reject(error);
+    }
+);
+
+// Response interceptor
+instance.interceptors.response.use(
+    (response) => {
+        console.log("Response data:", response.data);
+        return response;
+    },
+    (error) => {
+        console.error("Response error:", error);
+        if (error.response.status === 401) {
+            // Redirect to login or handle unauthorized error
+            console.log("Unauthorized error. Redirecting to login...");
+            alert("Please login again!");
+            localStorage.removeItem("userToken");
+            window.location.href = "/login"; // ✅ redirect using browser
+
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
